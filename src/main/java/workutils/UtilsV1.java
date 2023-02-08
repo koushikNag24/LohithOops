058c4ab052d7ee5bdc8909a95ab449e8fb8e486a
Lohith <lohithavj2001@gmail.com>
2023-02-08 16:44:01 +0530

    Learnt Strategy Pattern

--- src/main/java/workutils/UtilsV1.java
+++ src/main/java/workutils/UtilsV1.java
@@ -1,11 +1,13 @@
 package workutils;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class UtilsV1 implements IUtils {
     public int[] sort(int[] arr){
         ///asdddddd///
         Arrays.sort(arr);
+        Collections.sort(arr);
          return arr;
     }
     public void shout(){
