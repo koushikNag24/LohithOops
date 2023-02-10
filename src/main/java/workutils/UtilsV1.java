34b7834aea9f1d11f8fe0420590cecf78c524bec
Lohith <lohithavj2001@gmail.com>
2023-02-10 09:13:39 +0530

    Changes in UtilsV1

--- src/main/java/workutils/UtilsV1.java
+++ src/main/java/workutils/UtilsV1.java
@@ -1,13 +1,12 @@
 package workutils;
 
 import java.util.Arrays;
-import java.util.Collections;
 
 public class UtilsV1 implements IUtils {
     public int[] sort(int[] arr){
         ///asdddddd///
         Arrays.sort(arr);
-        Collections.sort(arr);
+        
          return arr;
     }
     public void shout(){
